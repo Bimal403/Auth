bf340bd8fc62fd7cb9491388ccaa4c8eeb59305a
BimalTimsina <bimaltimsina4@gmail.com>
2023-12-23 22:56:18 +0545

    express app created

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+const app = require("./src/config/express.config");
 const http = require("http");
 
-const server = http.createServer((req, res) => {
-  res.end("Hello world");
-});
-server.listen(3005, "localhost", () => {
-  console.log("Server is running on port 3005");
+const server = http.createServer(app);
+
+server.listen("3005", "localhost", (err) => {
+  if (!err) {
+    console.log("Server is running on port 3005");
+    console.log("Press Ctrl + C to disconnect");
+    console.log("Use http://localhost:3005/ to browse the server");
+  }
 });
