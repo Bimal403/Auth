ba9f22bd6c130ec4e86f49e525ef6623a1eb2bd6
BimalTimsina <bimaltimsina4@gmail.com>
2023-12-24 22:01:07 +0545

    router and middleware introduced

---
+++ readme.md
@@ -0,0 +1,20 @@
+## MVC
+
+### Routing
+
+- Defining the path for the url to perform actions
+- REST API
+
+  - Representational Stateless Transfer
+
+    - get, post, put, patch, delete
+
+    CRUD =>
+
+    Create ==========> Post
+    Read ============> Get
+    Update ==========> put/patch
+    delete ==========> delete
+
+  login request =====> token
+  webstorage token ===>Server
--- src/config/express.config.js
+++ src/config/express.config.js
@@ -1,4 +1,32 @@
 const express = require("express");
 const app = express();
 
+//routing
+app.get("/", (request, response, next) => {
+  response.json({
+    result: "hi",
+    msg: "success",
+    meta: null,
+  });
+});
+
+let checkLogin = (req, res, next) => {
+  let loggedIn = false;
+  if (loggedIn) {
+    next();
+  } else {
+    res.status(401).send("sorry");
+  }
+};
+
+app.get("/me", checkLogin, (req, res, next) => {
+  res.send("hi");
+});
+// app.post();
+// app.put();
+// app.patch();
+// app.delete();
+
+//app.use();
+
 module.exports = app;
